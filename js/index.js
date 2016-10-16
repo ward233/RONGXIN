87dbc19c7e74ea37bdbe7f4ef413c9e3d25f8620
ward233 <1095100728@qq.com>
2016-10-16 12:56:34 +0800

    添加了注释语句

--- js/index.js
+++ js/index.js
@@ -31,17 +31,22 @@ function LocalStorage() {
 
 var MylocalStorage = new LocalStorage();
 
+//首页轮播图的播放周期设置
 mui('#main-slider').slider({
 	interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
 });
+
+//设置标题栏的工具图标点击后打开侧栏
 mui(".mui-bar").on("tap", ".mui-action-menu", function() {
 	mui('.mui-off-canvas-wrap').offCanvas('show');
 });
 
+// 自定义滚动条的滚动系数设置
 mui('.mui-scroll-wrapper').scroll({
 	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 });
 
+// ajax的简化函数
 var ajaxFunc = function(urlText, successFun, errorFunc, data) {
 	jQuery.ajax({
 		url: urlText,
@@ -53,7 +58,9 @@ var ajaxFunc = function(urlText, successFun, errorFunc, data) {
 		success: successFun,
 		error: errorFunc
 	});
-}; // ajax简化函数
+};
+
+//标题栏的vue实例
 var headVm = new Vue({
 	el: "#header",
 	data: {
@@ -61,6 +68,7 @@ var headVm = new Vue({
 	}
 });
 
+//观察标题栏标题的变化从而动态曾加侧栏活动栏的样式 这里要重构
 headVm.$watch('h1Text', function(val) {
 	jQuery('.mui-off-canvas-left .mui-table-view-cell').filter(function() {
 		jQuery(this).removeClass('menu-active');
@@ -68,6 +76,8 @@ headVm.$watch('h1Text', function(val) {
 	}).addClass('menu-active');
 });
 
+
+//数据不存在时的显示模块
 var errorVm = new Vue({
 	el: '#show-error',
 	data: {
@@ -75,6 +85,7 @@ var errorVm = new Vue({
 	}
 });
 
+// 底部通用信息显示，这是一块独立显示的区域，会在所有页面显示
 var indexFooterVm = new Vue({
 	el: '#index-footer',
 	data: {
@@ -130,7 +141,10 @@ var indexFooterVm = new Vue({
 		}
 	}
 });
+
 indexFooterVm.dataInit();
+
+// 首页模块
 var mainVm = new Vue({ //主页的vue实例
 	el: 'main',
 	data: {
@@ -213,6 +227,7 @@ var mainVm = new Vue({ //主页的vue实例
 });
 mainVm.dataInit();
 
+//关于我们模块
 var aboutUsVm = new Vue({
 	el: '#about-us',
 	data: {
@@ -261,6 +276,7 @@ var aboutUsVm = new Vue({
 	}
 });
 
+//新闻中心模块
 var newsCenterVm = new Vue({
 	el: "#news-center",
 	data: {
@@ -317,6 +333,7 @@ var newsCenterVm = new Vue({
 	}
 });
 
+//企业服务模块
 var companyServiceVm = new Vue({
 	el: "#company-service",
 	data: {
@@ -405,6 +422,7 @@ var companyServiceVm = new Vue({
 	}
 });
 
+//企业案例模块
 var companyCaseVm = new Vue({
 	el: '#company-cases',
 	data: {
@@ -447,6 +465,7 @@ var companyCaseVm = new Vue({
 	}
 });
 
+//加入我们模块
 var joinUsVm = new Vue({
 	el: "#join-us",
 	data: {
@@ -486,8 +505,10 @@ var joinUsVm = new Vue({
 			errorVm.show = true;
 		}
 	}
-})
+});
 
+
+// 联系我们模块
 var contactUsVm = new Vue({
 	el: "#contact-us",
 	data: {
@@ -544,6 +565,8 @@ var db = openDatabase('testDB', '1.0', 'Test DB', 20 * 1024 * 1024);
 db.transaction(function(tx) {
 	tx.executeSql('CREATE TABLE IF NOT EXISTS news (typeId, content)');
 });
+
+//新闻详情模块
 var newsDetailVm = new Vue({
 	el: "#news-detail",
 	data: {
@@ -595,9 +618,13 @@ var newsDetailVm = new Vue({
 		}
 	}
 });
+
+//典型案例的数据表创建语句
 db.transaction(function(tx) {
 	tx.executeSql('CREATE TABLE IF NOT EXISTS cases (typeId, content)');
 });
+
+//典型案例模块
 var caseDetailVm = new Vue({
 	el: "#case-detail",
 	data: {
@@ -650,6 +677,7 @@ var caseDetailVm = new Vue({
 	}
 });
 
+//模块与文字的映射表
 var controlShow = {
 	"官方首页": mainVm,
 	"关于融信": aboutUsVm,
@@ -659,8 +687,9 @@ var controlShow = {
 	"人才招聘": joinUsVm,
 	"联系我们": contactUsVm,
 	"新闻详情": newsDetailVm,
-}
+};
 
+//侧栏切换功能语句
 mui(".mui-off-canvas-left").on("tap", ".mui-table-view-cell", function(event) {
 
 	controlShow[jQuery.trim(jQuery('.menu-active').text())].show = false;
@@ -672,6 +701,8 @@ mui(".mui-off-canvas-left").on("tap", ".mui-table-view-cell", function(event) {
 	mui('.mui-off-canvas-wrap').offCanvas().close();
 
 });
+
+//新闻详情显示语句块 此语句块除了显示新闻详情，还改变了标题栏的按钮的类
 mui(".news-list").on("tap", "a", function(event) {
 
 	newsDetailVm.prevName = jQuery.trim(jQuery('.menu-active').text());
@@ -681,6 +712,8 @@ mui(".news-list").on("tap", "a", function(event) {
 	jQuery("#header-menu").removeClass("mui-action-menu").addClass("mui-icon-arrowleft");
 	headVm.h1Text = "新闻详情";
 });
+
+// 给新闻详情和典型案例提供回退功能
 mui(".mui-bar").on("tap", ".mui-icon-arrowleft", function() {
 	if(headVm.h1Text === "新闻详情") {
 		newsDetailVm.show = false;
@@ -695,6 +728,8 @@ mui(".mui-bar").on("tap", ".mui-icon-arrowleft", function() {
 	jQuery("#header-menu").removeClass("mui-icon-arrowleft").addClass("mui-action-menu");
 
 });
+
+//典型案例显示语句块
 mui(".case-list").on("tap", "a", function(event) {
 
 	caseDetailVm.prevName = jQuery.trim(jQuery('.menu-active').text());
